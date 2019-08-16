531962f1f0514ec678a07b4fedfb694ed5764099
DHuan <huandzh@gmail.com>
2019-08-16 17:51:45 +0800

    Add AliyunApiGatewayClient. (#3)

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = "https://github.com/huandzh/restful-client-lite"
 EMAIL = "hd@iamhd.com"
 AUTHOR = "Huan Di"
 REQUIRES_PYTHON = ">=3.5.0"
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
